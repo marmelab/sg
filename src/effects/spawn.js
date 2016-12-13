e14602d842664bcef6a3f6032ea76a050e551d8a
ThieryMichel <thiery@marmelab.com>
2016-12-13 13:35:22 +0100

    remove generator verification from spawn effect, since sg already does it

--- src/effects/spawn.js
+++ src/effects/spawn.js
@@ -1,11 +1,7 @@
 import sg from '../sg';
 import createEffect from './createEffect';
-import isGenerator from '../utils/isGenerator';
 
 export const handleSpawnEffect = sgImpl => ([callable, ...args]) => {
-    if (isGenerator(callable)) {
-        throw new Error('spawn effect need a Generator');
-    }
     const promise = sgImpl(callable)(...args);
 
     return Promise.resolve(() => promise);

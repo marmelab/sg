7d8afefc0856ab1b8a4c532783e094c7c2e9d30a
ThieryMichel <thiery@marmelab.com>
2017-01-05 11:21:13 +0100

    add take_every effect

---
+++ src/effects/takeEvery.js
@@ -0,0 +1,15 @@
+import createEffect from './createEffect';
+import take from './take';
+import fork, { handleForkEffect } from './fork';
+
+export function* takeEverySaga(type, gen, ...args) {
+    while (true) {
+        const action = yield take(type);
+        yield fork(gen, ...args.concat(action));
+    }
+}
+
+export const handleTakeEveryEffect = ([type, gen, ...args], emitter, id) =>
+    handleForkEffect([takeEverySaga, type, gen, ...args], emitter, id);
+
+export default createEffect('takeEvery', handleTakeEveryEffect);
---
+++ src/effects/takeEvery.spec.js
@@ -0,0 +1,30 @@
+import expect from 'expect';
+
+import { takeEverySaga } from './takeEvery';
+import take from './take';
+import fork from './fork';
+
+describe('takeEverySaga', () => {
+    let iterator;
+    function* gen() {
+        yield Promise.resolve();
+    }
+    before(() => {
+        iterator = takeEverySaga('type', gen, 'arg1', 'arg2');
+    });
+
+    it('should call take with type', () => {
+        const next = iterator.next();
+        expect(next.value).toEqual(take('type'));
+    });
+
+    it('should call fork with args + value returned by take', () => {
+        const next = iterator.next('taken value');
+        expect(next.value).toEqual(fork(gen, 'arg1', 'arg2', 'taken value'));
+    });
+
+    it('should call take with type again', () => {
+        const next = iterator.next();
+        expect(next.value).toEqual(take('type'));
+    });
+});

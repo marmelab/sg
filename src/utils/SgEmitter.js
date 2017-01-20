d4b36b4a46f4d5b913399abae537b28776d56952
ThieryMichel <thiery@marmelab.com>
2017-01-20 09:27:40 +0100

    add test on sgEmitter

--- src/utils/SgEmitter.js
+++ src/utils/SgEmitter.js
@@ -1,41 +1,45 @@
 import without from 'lodash/without';
 import EventEmitter from 'events';
 
+export const puts = Symbol('puts');
+export const tasks = Symbol('tasks');
+export const takes = Symbol('takes');
+
 export default class SgEmitter extends EventEmitter {
     constructor() {
         super();
-        this.events = {};
-        this.tasks = [];
-        this.takes = [];
-        this.on('fork', ({ id }) => this.tasks.push(id));
+        this[puts] = {};
+        this[tasks] = [];
+        this[takes] = [];
+        this.on('fork', ({ id }) => this[tasks].push(id));
         this.on('cancel', ({ id }) => {
-            this.tasks = without(this.tasks, id);
+            this[tasks] = without(this[tasks], id);
         });
     }
+
     take(id, type) {
         const key = `${type}_${id}`;
-        const event = this.events[key];
+        const event = this[puts][key];
         if (event) {
-            delete this.events[key];
+            delete this[puts][key];
             return Promise.resolve(event);
         }
-        this.takes.push(id);
-
+        this[takes].push(id);
 
         return new Promise((resolve) => {
             this.once(type, (payload) => {
-                this.takes = without(this.takes, id);
+                this[takes] = without(this[takes], id);
                 resolve(payload);
             });
         });
     }
 
     put(id, type, payload) {
-        this.tasks
-        .filter(taskId => taskId !== id && this.takes.indexOf(id) === -1)
+        this[tasks]
+        .filter(taskId => taskId !== id && this[takes].indexOf(taskId) === -1)
         .map(taskId => `${type}_${taskId}`)
         .forEach((key) => {
-            this.events[key] = payload;
+            this[puts][key] = payload;
         });
         this.emit(type, payload);
     }
